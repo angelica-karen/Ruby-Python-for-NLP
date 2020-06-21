f3d4abab1e341945a8e9081a95802571dc5a4b76
angelica-karen <40410462+angelica-karen@users.noreply.github.com>
2020-06-21 11:25:59 +0800

    Initialise React, Python

---
+++ lib/assets/python/tokenizer.py
@@ -0,0 +1,9 @@
+import nltk.data
+
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+parag = "I love Jesus1. I love Jesus2. I love Jesus3. You are awesome."
+tokenizer.tokenize(parag)
+arrayT = tokenizer.tokenize(parag)
+tokenoutput = arrayT[2]
+
+print (tokenoutput)
